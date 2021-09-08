a67810d246f3cdc25808b8c1a7fd63c9a7a1559f
Muhammad Farhan Adib <muhammadfarhan2880@gmail.com>
2021-09-08 19:13:36 +0700

    Commit all file project

---
+++ assets/js/scripts.js
@@ -0,0 +1,73 @@
+//   all ------------------
+function initParadoxWay() {
+  "use strict";
+
+  if ($(".testimonials-carousel").length > 0) {
+    var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+      preloadImages: false,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      mousewheel: false,
+      centeredSlides: true,
+      pagination: {
+        el: ".tc-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: ".listing-carousel-button-next",
+        prevEl: ".listing-carousel-button-prev",
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  }
+
+  // bubbles -----------------
+
+  setInterval(function () {
+    var size = randomValue(sArray);
+    $(".bubbles").append(
+      '<div class="individual-bubble" style="left: ' +
+        randomValue(bArray) +
+        "px; width: " +
+        size +
+        "px; height:" +
+        size +
+        'px;"></div>'
+    );
+    $(".individual-bubble").animate(
+      {
+        bottom: "100%",
+        opacity: "-=0.7",
+      },
+      4000,
+      function () {
+        $(this).remove();
+      }
+    );
+  }, 350);
+}
+
+// Render Background Random Shape
+var html = "";
+for (var i = 1; i <= 50; i++) {
+  html +=
+    '<div class="shape-container--' +
+    i +
+    ' shape-animation"><div class="random-shape"></div></div>';
+}
+
+document.querySelector(".shape").innerHTML += html;
+
+var $allShapes = $("[class*='shape-container--']");
+
+//   Init All ------------------
+$(document).ready(function () {
+  initParadoxWay();
+});
